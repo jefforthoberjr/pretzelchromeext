172a3e5276a5b662946bdcbe054c9992c7f12c31
Allyson <allonsylotz@users.noreply.github.com>
2023-06-06 23:20:04 -0600

    create reading race extension

--- gorillas/scripts/content.js
+++ gorillas/scripts/content.js
@@ -1,22 +1,48 @@
-const article = document.querySelector("article");
+const article = document.querySelector("div.article-body")
 
-// `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
   const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const wordMatchRegExp = /[^\s]+/g;
   const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  const readingTimeLimitSeconds = Math.round(wordCount * 0.3);
+  const startDate = new Date();
+  const deadline = new Date(startDate.getTime() + (readingTimeLimitSeconds) * 1000);
+  console.log("Start Date is " + startDate);
+  console.log("Deadline is " + deadline);
+  
+  var badge = document.createElement("p");
+  //insert line about styling here
+  //badge.classList.add("color-secondary-text", "type--caption");
 
   // Support for API reference docs
-  const heading = article.querySelector("h1");
+  const authorBio = article.querySelector("div.has-bio");
   // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+  const authorOverlay = article.querySelector("div.author-overlay")?.parentNode;
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+  (authorBio ?? authorOverlay).insertAdjacentElement("afterend", badge);
+
+  var countdown = setInterval(function() {
+    var now = new Date().getTime();
+
+    var timeLeft = deadline - now;
+  
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
+  
+    badge.textContent = `⏱️ ${hours}:${minutes}:${seconds} to go!`
+  
+    // If the count down is finished, write some text
+    if (timeLeft < 0) {
+      clearInterval(countdown);
+      const html = document.querySelector("html");
+      html.remove();
+    }
+  }, 1000);
 }
